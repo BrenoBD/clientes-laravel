b1d5e1b7b780d4c0cde65543279a5793efb70fab
Breno Ferreira <brenoferreiraf2016@gmail.com>
2022-10-07 21:07:20 -0300

    Aprendendo laravel

---
+++ app/Http/Controllers/ClientesController.php
@@ -0,0 +1,17 @@
+<?php 
+
+	namespace App\Http\Controllers;
+	use Illuminate\Http\Request;
+
+	/**
+	 * 
+	 */
+	class ClientesController extends Controllers
+	{
+		
+		function index()
+		{
+			echo ' heelo';
+		}
+	}
+?>
